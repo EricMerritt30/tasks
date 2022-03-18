e742c547783ffa987d03cc9484663bb0e6409467
Eric Merritt <ericmero@udel.edu>
2022-03-18 12:49:23 -0400

    completed a few of the function!

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,8 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    temperature = ((temperature - 32) * 5) / 9;
+    return temperature;
 }
 
 /**
@@ -12,7 +13,18 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum = 0;
+    if (first > 0) {
+        sum += first;
+    }
+    if (second > 0) {
+        sum += second;
+    }
+    if (third > 0) {
+        sum += third;
+    }
+
+    return sum;
 }
 
 /**
@@ -20,7 +32,8 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    const newString = message.toLocaleUpperCase;
+    return newString + "!";
 }
 
 /**
@@ -28,7 +41,8 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    const ending = message.endsWith("?");
+    return ending;
 }
 
 /**
