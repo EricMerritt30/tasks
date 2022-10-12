c0ffdb49bc15088160b873d4cfc50433a1fbf180
Eric Merritt <ericmero@udel.edu>
2022-10-12 18:41:03 -0400

    finished the Chage Type

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,19 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [kind, setType] = useState<QuestionType>("short_answer_question");
+    function changeType(): void {
+        setType(
+            kind === "multiple_choice_question"
+                ? "short_answer_question"
+                : "multiple_choice_question"
+        );
+    }
+    return (
+        <div>
+            <Button onClick={changeType}>Change Type</Button>
+            {kind === "multiple_choice_question" && <div>Multiple Choice</div>}
+            {kind === "short_answer_question" && <div>Short Answer</div>}
+        </div>
+    );
 }
